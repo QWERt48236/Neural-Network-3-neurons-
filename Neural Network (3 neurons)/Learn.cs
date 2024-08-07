2799431b1d9fc8714dab12eab4172ce919355f91
QWERt48236 <126250411+QWERt48236@users.noreply.github.com>
2024-08-07 12:02:43 +0300

    change

--- Neural Network (3 neurons)/Learn.cs
+++ Neural Network (3 neurons)/Learn.cs
@@ -8,42 +8,55 @@ namespace Neural_Network__3_neurons_
 {
     public class NeuralNetwork
     {
-        double w000 = 0.7;
-        double w010 = 0.3;
-        double w001 = 0.6;
-        double w011 = 0.4;
-        double w10 = 0.8;
-        double w11 = 0.2;
+        double w000 = 0.2;
+        double w010 = -0.8;
+        double w001 = 0.4;
+        double w011 =- 0.6;
+        double w10 = 0.3;
+        double w11 = -0.7;
+
+
+        double NeuronActivation(double x1, double x2, double w1, double w2) 
+        {
+            return 1 / (1 + Math.Exp(-(x1 * w1 + x2 * w2)));
+        }
 
         double ExitNeuronSum(double x1, double x2)
         {
-            return ((x1 * w000 + x2 * w010) * w10 + (x1 * w001 + x2 * w011) * w11);
+            double neuron1 = 1 / (1 + Math.Exp(-(x1 * w000 + x2 * w010)));
+            double neuron2 = 1 / (1 + Math.Exp(-(x1 * w001 + x2 * w011)));
+
+            return (neuron1 * w10 + neuron2 * w11);
         }
 
         double ForwardPropagation(double x1, double x2)
         {
-            double y = 1 / (1 + Math.Exp(-ExitNeuronSum(x1, x2)));
+            double neuron1 = 1 / (1 + Math.Exp(-(x1 * w000 + x2 * w010)));
+            double neuron2 = 1 / (1 + Math.Exp(-(x1 * w001 + x2 * w011)));
+
+            double y = 1 / (1 + Math.Exp(-(neuron1 * w10 + neuron2 * w11)));
 
             return y;
         }
 
         void UpdateWeights(double Yexpected, double x1, double x2, double learningRate)
         {
-            w000 = w000 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * Math.Pow(ForwardPropagation(x1, x2), 2) * Math.Exp(ExitNeuronSum(x1, x2)) * w10 * x1);
-            w010 = w010 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * Math.Pow(ForwardPropagation(x1, x2), 2) * Math.Exp(ExitNeuronSum(x1, x2)) * w10 * x2);
-            w001 = w001 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * Math.Pow(ForwardPropagation(x1, x2), 2) * Math.Exp(ExitNeuronSum(x1, x2)) * w11 * x1);
-            w011 = w011 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * Math.Pow(ForwardPropagation(x1, x2), 2) * Math.Exp(ExitNeuronSum(x1, x2)) * w11 * x2);
-            w10 = w10 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * Math.Pow(ForwardPropagation(x1, x2), 2) * Math.Exp(ExitNeuronSum(x1, x2)) * (-(x1 * w000 + x2 * w010)));
-            w11 = w11 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * Math.Pow(ForwardPropagation(x1, x2), 2) * Math.Exp(ExitNeuronSum(x1, x2)) * (-(x1 * w001 + x2 * w011)));
+            w000 = w000 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * ForwardPropagation(x1, x2) * NeuronActivation(x1, x2, w000, w010) * Math.Pow(ForwardPropagation(x1, x2), 2) * (-w10) * x1);
+            w010 = w010 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * ForwardPropagation(x1, x2) * NeuronActivation(x1, x2, w000, w010) * Math.Pow(ForwardPropagation(x1, x2), 2) * (-w10) * x2);
+            w001 = w001 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * ForwardPropagation(x1, x2) * NeuronActivation(x1, x2, w001, w011) * Math.Pow(ForwardPropagation(x1, x2), 2) * (-w11) * x1);
+            w011 = w011 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * ForwardPropagation(x1, x2) * NeuronActivation(x1, x2, w001, w011) * Math.Pow(ForwardPropagation(x1, x2), 2) * (-w11) * x2);
+            w10 = w10 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * ForwardPropagation(x1, x2) * NeuronActivation(x1, x2, w000, w010) * Math.Pow(ForwardPropagation(x1, x2), 2));
+            w11 = w11 - learningRate * (2 * (ForwardPropagation(x1, x2) - Yexpected) * ForwardPropagation(x1, x2) * NeuronActivation(x1, x2, w001, w011) * Math.Pow(ForwardPropagation(x1, x2), 2));
         }
 
         double NetworkErrorFunction(double[] x1, double[] x2, double[] Yexpected)
         {
             double sum = 0;
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < x1.Length; i++)
             {
                 sum += ForwardPropagation(x1[i], x2[i]) - Yexpected[i];
+                Console.WriteLine(ForwardPropagation(x1[i], x2[i]) - Yexpected[i]);
             }
 
             return sum/ x1.Length;
--- Neural Network (3 neurons)/Program.cs
+++ Neural Network (3 neurons)/Program.cs
@@ -2,15 +2,15 @@
 
 using Neural_Network__3_neurons_;
 
-double[] x1 = { 0.5, 0.4, 0.5, 0.9, 0.5 };
-double[] x2 = { 0.5, 0.5, 0.3, 0.5, 0.1 };
+double[] x1 = { 0.9, 0.2, 0.7, 0.9, 0.8, 0.2, 0.7 };
+double[] x2 = { 0.1, 0.8, 0.3, 0.1, 0.2, 0.8, 0.3 };
 
-double[] Yexpected = { 1, 0, 1, 1, 1 };
+double[] Yexpected = { 1, 0, 1, 1, 1, 0, 0, 0};
 
 double learningRate = 0.1;
 
 NeuralNetwork neural = new NeuralNetwork();
 
-neural.Learn(x1, x2, Yexpected, learningRate, 10);
+neural.Learn(x1, x2, Yexpected, learningRate, 1000);
 
 
